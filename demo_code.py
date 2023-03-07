b0cd242c2a2144ab0917a8ea2a1c71bc5e867d21
ayertey01 <109234378+ayertey01@users.noreply.github.com>
2023-03-07 21:53:01 +0000

    Update demo_code.py

--- demo_code.py
+++ demo_code.py
@@ -4,3 +4,10 @@ df=pd.read_csv('file directory')
 df
 # using an attribute to find the number of columbs and rows
 df.shape
+#using the .info() to provide information about the csv file
+df.info()
+#to display all the columbs use
+pd.set_option('display.max_columns', number_of_column)
+#using a schema 
+schema_df= pd.read_csv('directory')
+schema_df
