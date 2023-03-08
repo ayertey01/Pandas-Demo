1118c01bb6a8e1e455bd3a91ea016f3210ea8d93
ayertey01 <109234378+ayertey01@users.noreply.github.com>
2023-03-08 22:39:24 +0000

    Update demo_code.py

--- demo_code.py
+++ demo_code.py
@@ -11,3 +11,8 @@ pd.set_option('display.max_columns', number_of_column)
 #using a schema 
 schema_df= pd.read_csv('directory')
 schema_df
+#the use of head and tail can be incorporated in  the code 
+schema_df.tail(10)
+#showing last 10 of the schema
+shema_df.head(5)
+#showing first 5 of the schema
